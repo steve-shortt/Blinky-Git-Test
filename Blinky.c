f5d4ecc89d42342c2e7d40b1ebd0717efa65eaa5
steve-shortt <stephen@joraltechnologies.com>
2025-08-20 09:17:49 -0400

    dddd

--- Blinky.c
+++ Blinky.c
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  * 
- * more comments zzzzzzzzzzzzqq
+ * more comments zzzzzzzzzzzz
  *---------------------------------------------------------------------------*/
 
 #include <stdio.h>
